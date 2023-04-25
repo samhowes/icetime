36cfc25eae60ed6414cf110be2ce46b450f65d02
Sam Howes <sam@samhowes.com>
2023-04-25 09:19:16 -0400

    init @angular/fire

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
+  firebase: {
+    projectId: 'icetime-8060f',
+    appId: '1:252002904555:web:04b187be5b190ef09d11ae',
+    databaseURL: 'https://icetime-8060f-default-rtdb.firebaseio.com',
+    storageBucket: 'icetime-8060f.appspot.com',
+    apiKey: 'AIzaSyCpVpUIMYRzfJp6cBYVPUXpN81zcgg78Eg',
+    authDomain: 'icetime-8060f.firebaseapp.com',
+    messagingSenderId: '252002904555',
+    measurementId: 'G-BLZW56L2SB',
+  },
   production: true
 };
