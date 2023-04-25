4ca8deab8679c0b877abca801d0fe91ac7e8c550
Sam Howes <sam@samhowes.com>
2023-04-25 09:30:00 -0400

    Setup angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
 import { environment } from '../environments/environment';
 import { provideAuth,getAuth } from '@angular/fire/auth';
 import { provideDatabase,getDatabase } from '@angular/fire/database';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MaterialModule} from "./material.module";
 
 @NgModule({
   declarations: [
@@ -17,7 +19,9 @@ import { provideDatabase,getDatabase } from '@angular/fire/database';
     AppRoutingModule,
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideAuth(() => getAuth()),
-    provideDatabase(() => getDatabase())
+    provideDatabase(() => getDatabase()),
+    BrowserAnimationsModule,
+    MaterialModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/material.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import {MatToolbarModule} from "@angular/material/toolbar";
+
+@NgModule({
+  imports: [],
+  exports: [
+    MatToolbarModule,
+  ]
+})
+export class MaterialModule { }
